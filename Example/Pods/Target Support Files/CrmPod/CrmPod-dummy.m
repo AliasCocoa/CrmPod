2a30834772deafe5d03559223649e4e917ddc9cf
AliasCocoa <cocoaalias@gmail.com>
2017-12-12 15:15:16 +0100

    first depo

---
+++ Example/Pods/Target Support Files/CrmPod/CrmPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CrmPod : NSObject
+@end
+@implementation PodsDummy_CrmPod
+@end
---
+++ Example/Pods/Target Support Files/Pods-CrmPod_Example/Pods-CrmPod_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CrmPod_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CrmPod_Example
+@end
